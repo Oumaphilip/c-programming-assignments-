a0859fe3665d6e57155c75e60f526f76eb7e4809
Oumaphilip <114051683+Oumaphilip@users.noreply.github.com>
2022-10-03 11:58:20 +0300

    Add files via upload

---
+++ philip c++,sum.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int i,x,y,sum=0;
+	cout<<"enter the starting point"<<endl;
+	cin>>x;
+	cout<<"enter stoping point"<<endl;		
+	cin>>y;
+for(i=x;i<=y;i++){
+	cout<<"i"<<endl;
+sum+=i
+}
+cout<<"the sum of numbers between x and y"<<"is"<<sum;
+}
+return 0;
+}
