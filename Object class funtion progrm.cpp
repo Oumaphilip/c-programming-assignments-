24560cf35d07f258c7dd02ae5a20c278e0262965
Oumaphilip <114051683+Oumaphilip@users.noreply.github.com>
2022-11-16 14:51:05 +0300

    Add files via upload

---
+++ Object class funtion progrm.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespaces std;
+class Room{
+	public:
+		double length;
+		double width;
+		double breadth;
+	double calculateArea(){
+		return length*width
+	}
+	double calculateVolume(){
+		return length*width*breadth
+	}
+};
+	int main(){
+			Room room1
+			room1.length=42.5
+			room1.width=30.8
+			room1.breadth=19.2
+			cout<<"area of room="<< room1calculateArea()<<endl;
+			cout<<"Volumeof room="<<room1calculateVolume()<<endl;
+	}
+			return 0,
+}
